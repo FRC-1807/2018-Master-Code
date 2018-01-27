464e107d08191b521df0fd18c39ad4ae2e783c17
Peter <peterrauscher18@gmail.com>
2018-01-27 11:04:31 -0500

    big amogh penis
    brown

--- src/org/usfirst/frc/team1807/robot/Robot.java
+++ src/org/usfirst/frc/team1807/robot/Robot.java
@@ -67,7 +67,7 @@ public class Robot extends IterativeRobot {
 	Talon leftBack;
 	Talon rightFront;
 	Talon rightBack;
-	DifferentialDrive chassis;
+	DifferentialDrive bigamoghdick;
 	SpeedControllerGroup leftAmogh;
 	SpeedControllerGroup rightAmogh;
 	public static int LF;
@@ -115,7 +115,7 @@ public class Robot extends IterativeRobot {
 		rightBack = new Talon(RB);
 		leftAmogh = new SpeedControllerGroup(leftFront, leftBack);
 		rightAmogh = new SpeedControllerGroup(rightFront, rightBack);
-		chassis = new DifferentialDrive(leftAmogh, rightAmogh);
+		bigamoghdick = new DifferentialDrive(leftAmogh, rightAmogh);
 
 		//Pneumatics
 		/*compressor = new Compressor(0);
@@ -168,7 +168,7 @@ public class Robot extends IterativeRobot {
 			playing = true;
 			while(playing){
 				if(play_counter <= movementLinear.size() - 1) {
-					chassis.arcadeDrive(movementLinear.get(play_counter), movementRotate.get(play_counter));
+					bigamoghdick.arcadeDrive(movementLinear.get(play_counter), movementRotate.get(play_counter));
 					play_counter++;
 				} else {
 					playing = false;
@@ -265,7 +265,7 @@ public class Robot extends IterativeRobot {
 
 		if(playing){
 			if(play_counter <= movementLinear.size() - 1) {
-				chassis.arcadeDrive(movementLinear.get(play_counter), movementRotate.get(play_counter));
+				bigamoghdick.arcadeDrive(movementLinear.get(play_counter), movementRotate.get(play_counter));
 				play_counter++;
 			} else {
 				playing = false;
